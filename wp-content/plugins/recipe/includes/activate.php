5874703403dff8a727898ed164cac2787e36374e
devleopar <oktarkara@gmail.com>
2020-05-03 00:56:53 +0300

    sqls

--- wp-content/plugins/recipe/includes/activate.php
+++ wp-content/plugins/recipe/includes/activate.php
@@ -5,6 +5,20 @@ function r_activate_plugin(){
         wp_die( __('You must update Wordpress to join the lightside','recipe') );
     }
 
+    global $wpdb;
+    $createSQL          = "
+    CREATE TABLE `". $wpdb->prefix ."recipe_ratings` (
+        `ID` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+        `recipe_id` BIGINT(20) UNSIGNED NOT NULL DEFAULT '0',
+        `rating` FLOAT(3,2) UNSIGNED NOT NULL DEFAULT '0.00',
+        `user_ip` VARCHAR(50) NOT NULL DEFAULT '' COLLATE 'utf8mb4_general_ci',
+        PRIMARY KEY (`ID`) USING BTREE
+    )
+    ENGINE=InnoDB " . $wpdb->get_charset_collate() .";";
+
+    require( ABSPATH . "/wp-admin/includes/upgrade.php" );
+    dbDelta( $createSQL );
+
 }
 
 
