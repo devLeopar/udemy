9056f0062531e63035665aa9d30c20cf066f7608
devleopar <oktarkara@gmail.com>
2020-05-03 21:14:08 +0300

    ratings add to admin

--- wp-content/plugins/recipe/index.php
+++ wp-content/plugins/recipe/index.php
@@ -1,12 +1,12 @@
-<?php 
+<?php
 /*
-* Plugin Name: Recipe
-* Description: A simple Wordpress plugin that allows user to create recipes and rate those funny things
-* Version: 1.0
-* Author: Devleopar
-* Author URI: https://github.com/devLeopar
-* Text Domain: recipe
-*/
+ * Plugin Name: Recipe
+ * Description: A simple WordPress plugin that allows user to create recipes and rate those recipes
+ * Version: 1.0
+ * Author: Udemy
+ * Author URI: https://udemy.com
+ * Text Domain: recipe
+ */
 
 if( !function_exists( 'add_action' ) ){
     echo "Hi there! I'm just a plugin, not much I can do when called directly.";
@@ -21,19 +21,18 @@ include( 'includes/activate.php' );
 include( 'includes/init.php' );
 include( 'process/save-post.php' );
 include( 'process/filter-content.php' );
-include( 'process/rate-recipe.php' );
 include( 'includes/front/enqueue.php' );
+include( 'process/rate-recipe.php' );
+include( 'includes/admin/init.php' );
 
 // Hooks
-register_activation_hook( __FILE__,'r_activate_plugin');
+register_activation_hook( __FILE__, 'r_activate_plugin' );
 add_action( 'init', 'recipe_init' );
-add_action( 'save_post_recipe', 'r_save_post_admin',10,3 );
-add_filter('the_content','r_filter_recipe_content');
-add_action('wp_enqueue_scripts','r_enqueue_scripts',100);
-add_action('wp_ajax_r_rate_recipe','r_rate_recipe');
-add_action('wp_ajax_nopriv_r_rate_recipe','r_rate_recipe');
+add_action( 'save_post_recipe', 'r_save_post_admin', 10, 3 );
+add_filter( 'the_content', 'r_filter_recipe_content' );
+add_action( 'wp_enqueue_scripts', 'r_enqueue_scripts', 100 );
+add_action( 'wp_ajax_r_rate_recipe', 'r_rate_recipe' );
+add_action( 'wp_ajax_nopriv_r_rate_recipe', 'r_rate_recipe' );
+add_action( 'admin_init', 'recipe_admin_init' );
 
-// Shortcode
-
-
-?>
+// Shortcodes
