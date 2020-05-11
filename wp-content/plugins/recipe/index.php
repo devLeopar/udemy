23c4fc020492a4b9a6ce58bfbd1978eab20a1e01
devleopar <oktarkara@gmail.com>
2020-05-11 17:34:51 +0300

    dashboard widget added

--- wp-content/plugins/recipe/index.php
+++ wp-content/plugins/recipe/index.php
@@ -31,13 +31,13 @@ include( 'includes/cron.php' );
 include( 'includes/deactivate.php' );
 include( 'includes/utility.php' );
 include( 'includes/shortcodes/creator.php' );
-include( 'includes/shortcodes/auth-form.php' );
 include( 'process/submit-user-recipe.php' );
+include( 'includes/shortcodes/auth-form.php' );
 include( 'process/create-account.php' );
 include( 'process/login.php' );
 include( 'includes/shortcodes/auth-alt-form.php' );
 include( 'includes/front/logout-link.php' );
-
+include( 'includes/admin/dashboard-widgets.php' );
 // Hooks
 register_activation_hook( __FILE__, 'r_activate_plugin' );
 register_deactivation_hook( __FILE__, 'r_deactivate_plugin' );
@@ -60,6 +60,8 @@ add_action( 'wp_ajax_nopriv_recipe_user_login', 'recipe_user_login' );
 // add_filter( 'authenticate', 'wp_authenticate_spam_check', 99 );
 // add_filter( 'authenticate', 'r_alt_authenticate', 100, 3 );
 add_filter( 'wp_nav_menu_secondary_items', 'ju_new_nav_menu_items', 999 );
+add_action( 'wp_dashboard_setup', 'r_dashboard_widgets' );
+
 
 // Shortcodes
 add_shortcode( 'recipe_creator', 'r_recipe_creator_shortcode' );
